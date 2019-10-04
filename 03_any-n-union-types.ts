d9efbb81fdf8e6b2aaf6f274969b295762a372a1
Shubat Max <shubat.max@gmail.com>
2019-10-04 22:42:19 +0300

    Type 'any' and union types

---
+++ 03_any-n-union-types.ts
@@ -0,0 +1,19 @@
+/**
+ * TypeScript has a possibility to disable all the type checking by using type <<any>>
+ *     this might provide your script with some flexibility when needed
+ */
+
+let a: any;
+
+a = 10;
+a = true;
+a = 'any string';
+
+/**
+ * TypeScript also has possibility to make type checking for more then 1 type using pipe symbol << | >>
+ */
+
+let b: number | string;
+b = 10;
+// b = false; // => <<TS2322: Type 'false' is not assignable to type 'string | number'. Alt+Shift+Enter Alt+Enter>>
+b = 'any string';
