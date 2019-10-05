7158679aa78a011d2582b5716489f1bd5052f016
Shubat Max <shubat.max@gmail.com>
2019-10-05 21:00:07 +0300

    Typescript Generics

---
+++ 09_generics.ts
@@ -0,0 +1,57 @@
+export {};
+
+function echo0(arg: any): any {
+  return arg;
+}
+
+let myStr0: string = echo0(1); // This will work because TS is allowing this since the return type is <<any>>
+
+/**
+ * Generics
+ */
+function echo<T>(arg: T): T {
+  return arg;
+}
+
+// let myStr: string = echo(1); // <<TS2322: Type '1' is not assignable to type 'string'>>
+let myStr: number = echo(1);
+
+class Person {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getFullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+class Admin extends Person {}
+
+class Manager extends Person {}
+
+let admin = new Admin("admin", "admin");
+let manager = new Manager("manager", "manager");
+
+// function personEcho<T>(person: T): T {
+//   person.firstName;
+//   return person;
+// }
+// let foo = personEcho(admin); // let foo: Admin - don't have an access to Person properties
+
+// function personEcho(person: Person): Person {
+//   person.firstName;
+//   return person;
+// }
+// let foo = personEcho(admin); // let foo: Person - have an access to Person properties
+
+function personEcho<T extends Person>(person: T): T {
+  // person.firstName;
+  return person;
+}
+let foo = personEcho(admin); // let foo: Admin - have an access to Person properties
+let bar = personEcho(manager); // let foo: Manager - have an access to Person properties
